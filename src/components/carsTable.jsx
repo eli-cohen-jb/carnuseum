8543dd258c9dcc3941682c12640799cdbde4fae6
Eli Cohen <eli.cohen.jb@gmail.com>
2022-06-23 14:37:06 +0300

    refactor - extracting table body

--- src/components/carsTable.jsx
+++ src/components/carsTable.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Like from "./common/like";
 import TableHeader from "./common/tableHeader";
+import TableBody from "./common/tableBody";
 
 class CarsTable extends Component {
   state = {};
@@ -8,49 +9,46 @@ class CarsTable extends Component {
     { path: "mileage", label: "Mileage" },
     { path: "make", label: "Make" },
     { path: "model", label: "Model" },
-    { path: "fuel", label: "Fuel" },
+    { path: "fuel.name", label: "Fuel" },
     { path: "gear", label: "Gear" },
     { path: "offerType", label: "OfferType" },
     { path: "price", label: "Price" },
     { path: "hp", label: "Hp" },
     { path: "year", label: "Year" },
-    { key: 'like'},
-    { key:'delete'},
+    {
+      key: "like",
+      content: (car) => (
+        <Like liked={car.liked} onClick={() => this.props.onLike(car)} />
+      ),
+    },
+    {
+      key: "delete",
+      content: (car=>
+        <button
+          onClick={() => this.props.onDelete(car)}
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
+      ),
+    },
   ];
 
   render() {
     const { cars, onDelete, onSort, onLike, sortColumn } = this.props;
     return (
       <table className="table">
-         <TableHeader columns={this.columns} onSort={onSort} sortColumn={sortColumn}></TableHeader>
-        <tbody>
-          {cars.map((car) => {
-            return (
-              <tr key={car.id}>
-                <td>{car.mileage}</td>
-                <td>{car.make}</td>
-                <td>{car.model}</td>
-                <td>{car.fuel}</td>
-                <td>{car.gear}</td>
-                <td>{car.offerType}</td>
-                <td>{car.price}</td>
-                <td>{car.hp}</td>
-                <td>{car.year}</td>
-                <td>
-                  <Like liked={car.liked} onClick={() => onLike(car)} />
-                </td>
-                <td>
-                  <button
-                    onClick={() => onDelete(car)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
+        <TableHeader
+          columns={this.columns}
+          onSort={onSort}
+          sortColumn={sortColumn}
+        ></TableHeader>
+        <TableBody
+          columns={this.columns}
+          data={cars}
+          onLike={onLike}
+          onDelete={onDelete}
+        ></TableBody>        
       </table>
     );
   }
---
+++ src/components/common/tableBody.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Like from "./like";
+import _ from "lodash";
+
+class TableBody extends Component {
+  state = {};
+  renderCell = (item, column) => {
+    if (column.content) return column.content(item);
+
+    return _.get(item, column.path);
+  };
+
+  createKey = (item, column) => {
+    return item.id + (column.path || column.key);
+  };
+
+  render() {
+    const { data, columns } = this.props;
+
+    return (
+      <tbody>
+        {data.map((item) => (
+          <tr key={item.id}>
+            {columns.map((column) => (
+              // <td>{item[column.path]}</td>
+              <td key={this.createKey(item, column)}>
+                {this.renderCell(item, column)}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    );
+  }
+}
+
+export default TableBody;
